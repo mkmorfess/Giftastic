d4e3c9b269312f7201498c2e87956041534df8e0
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-18 13:46:01 -0500

    slight changes

--- assets/gif.js
+++ assets/gif.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 
 	for (var i = 0; i < topics.length; i++) {
 
-		$(".button").append("<button id='" + topics[i] + "' class='btn btn-primary click'>" + topics[i] + "</button>")
+		$(".button").append("<button class='btn btn-primary click'>" + topics[i] + "</button>")
 	
-	$("#" + topics[i]).on("click", function() {
+	$(".click").on("click", function() {
 
 				console.log("its working!")
 
@@ -25,24 +25,20 @@ $(document).ready(function(){
 
 			if (idx === array.length - 1) {
 				
-			$(".button").append("<button id='" + i + "' class='btn btn-primary click'>" + i + "</button>")
+			$(".button").append("<button class='btn btn-primary click'>" + i + "</button>")
 
-				$("#" + i).on("click", function() {
+			console.log(topics)
+
+				$(".click").on("click", function() {
 
 				console.log("its working!")
+
 
 			})
 			};
 			
 		});
 	});
-
-// $("#" + topics[i]).on("click", function() {
-
-// 				console.log("its working!")
-
-
-// 			});
 
 // var url = "api.giphy.com/v1/gifs/search?api_key=lv3qfDrkQTRe2crwKMOu3a11pwpDGlVy&limit=9&q="
 
