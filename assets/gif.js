7c7e1db591c2c7353a34d1ca93269265c8b812a3
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-19 06:09:18 -0500

    updated

--- assets/gif.js
+++ assets/gif.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
 
-	var topics = ["People", "Animals", "Garden", "Games"];
+var topics = ["People", "Animals", "Garden", "Games"];
+
+function initialButtons() {
+
+$("#button").empty();
+
 
 
 	for (var i = 0; i < topics.length; i++) {
 
-		$(".button").append("<button class='btn btn-primary click'>" + topics[i] + "</button>")
+		var button = $("<button>");
+		button.addClass("btn btn-primary click");
+		button.attr("data-name", topics[i]);
+		button.text(topics[i]);
+		$("#button").append(button);
 
 	};
 
+};
 
 	$("#form").submit(function(event) {
 		event.preventDefault();
-		var selection = $("#input").val()
+		var selection = $("#input").val().trim()
 		topics.push(selection)
+		console.log(topics)
 
-		topics.forEach(function(i, idx, array) {
-
-			if (idx === array.length - 1) {
-				
-			$(".button").append("<button class='btn btn-primary click'>" + i + "</button>")
-
-			console.log(topics)
-
-				
-			};
-			
-		});
+		initialButtons();
 	});
-
 
 	$(".click").on("click", function() {
 
@@ -37,7 +36,37 @@ $(document).ready(function(){
 
 			})
 
-// var url = "api.giphy.com/v1/gifs/search?api_key=lv3qfDrkQTRe2crwKMOu3a11pwpDGlVy&limit=9&q="
+
+	function displayGif() {
+
+        var gif = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lv3qfDrkQTRe2crwKMOu3a11pwpDGlVy&limit=9&q=" + topics;
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+
+        	// console.log(response)
+
+        	for (var i = 0; i < 9; i++) {
+
+        		console.log(response.data)
+
+          $("#gifs").text(JSON.stringify(response.data.i.url));
+
+      }
+          initialButtons();
+        });
+      }
+
+
+
+$(document).on("click", ".click", displayGif);
+
+	initialButtons();
+
+// var url = "https://api.giphy.com/v1/gifs/search?api_key=lv3qfDrkQTRe2crwKMOu3a11pwpDGlVy&limit=9&q="
 
 // for (var i = 0; i < topics.length; i++) {
 // $.ajax({
